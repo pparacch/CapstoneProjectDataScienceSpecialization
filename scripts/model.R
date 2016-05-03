a83ad171bf98e74307b1960628d612dde8b084c2
Pier Lorenzo Paracchini <PPAR@statoil.com>
2016-05-03 20:38:30 +0200

    Bigram model with supporting functions & unit tests

--- scripts/model.R
+++ scripts/model.R
@@ -32,10 +32,14 @@ load.twitter.3g.data <- function(folder){
     list(tdm = twitter.corpora.tdm.3g, allTermsCounters = twitter.allTerms.3g)
 }
 
+########################
+####### UNIGRAMS #######
+########################
+
 
 unigrams.countForWord <- function(word, u.words, u.counters){
     idx <- which(u.words == word)
-    result <- -1
+    result <- 0
     if(length(idx) == 1){
         result <- u.counters[which(u.words == word)]
     }else{
@@ -75,3 +79,71 @@ unigrams.model <- function(u.words, u.counters){
     result_ls <- list(u.words = result.words, u.probability = result.prob)
 }
 
+#######################
+####### BIGRAMS #######
+#######################
+
+bigrams.countForTerm <- function(term, b.terms, b.counters){
+    idx <- which(b.terms == term)
+    result <- 0
+    if(length(idx) == 1){
+        result <- b.counters[idx]
+    }else{
+        stop(paste("bigrams.countForTerm function::", term, "::not found", sep = ""))
+    }
+    result
+}
+
+#Get the probability for a birgrams
+# term -> the bigrams as "word_i-1 word_i"
+# $b.terms -> the list of bigrams (language model)
+# $b.counters -> the list of counts for each bigram
+# $u.words -> the list of unigramss (language model)
+# $u.counters -> the list of counts for each unigram
+# Note! for bigram b.terms[i], respective count is b.counters[i]
+# Note! for unigram u.words[i], respective count is u.counters[i]
+
+# Return probability for the term (bigram)
+# The probability for term = "w_i-1 wi" is calculated as P(w_i|w_i-1) = count(w_i-1, w_i)/ count(w_i-1)
+
+bigrams.probabilityForTerm <- function(term, b.terms, b.counters, u.words, u.counters){
+    result <- -1
+    idx <- which(b.terms == term)
+    term.words <- ngramTokenize(y = term, ng = 1)
+
+    if(length(idx) != 0){
+        w.previous <- term.words[1]
+        w.previous.count <- unigrams.countForWord(word = w.previous, u.words = u.words, u.counters = u.counters)
+        result <- b.counters[which(b.terms == term)]/ w.previous.count
+    }else{
+        stop(paste("bigrams.probabilityForTerm function::", term, "::not found", sep = ""))
+    }
+    result
+}
+
+# Create the Bigram model as a list
+# $b.word -> the list of word (w_i-1)
+# $b.nextWord -> the list of word (w_i)
+# $b.probability -> the list of probabilities
+#
+# Note!! $b.probability[i] (probability) is related to "$b.word[i] $b.nextWord[i]"(bigram)
+# The probability for term = "w_i-1 wi" is calculated as P(w_i|w_i-1) = count(w_i-1, w_i)/ count(w_i-1)
+
+bigrams.model <- function(b.terms, b.counters, u.words, u.counters){
+    size <- length(b.terms)
+    result.word <- character(size)
+    result.nextWord <- character(size)
+    result.prob <- numeric(size)
+    
+    for(i in 1:size){
+        tmp <- b.terms[i]
+        tmp.words <- ngramTokenize(y = tmp, ng = 1)
+        result.word[i] <- tmp.words[1]
+        result.nextWord[i] <- tmp.words[2]
+        result.prob[i] <- bigrams.probabilityForTerm(term = tmp, 
+                                                    b.terms = b.terms, b.counters = b.counters,
+                                                    u.words = u.words, u.counters = u.counters)
+    }
+    
+    result_ls <- list(b.word = result.word, b.nextWord = result.nextWord,  b.probability = result.prob)
+}
--- scripts/model_test.R
+++ scripts/model_test.R
@@ -1,10 +1,13 @@
+rm(list = ls())
 source("model.R")
+
+print("##########################################")
+print("###############Unigrams###################")
+print("##########################################")
 
 u.test.words <- c("the", "time", "of", "love")
 u.test.counter <- c(10, 20, 30, 40)
 u.text.expected.prob <- c(0.1, 0.2, 0.3, 0.4)
-
-print("##########################################")
 
 u.test.input.words <- c("the", "time", "of", "love")
 for(i in 1:length(u.test.input.words)){
@@ -61,4 +64,84 @@ check1 <- all(test.result$u.words == u.test.words)
 check2 <- all(test.result$u.probability == u.text.expected.prob)
 print(paste(test.name, "::unigrams.model::", (check1 & check2), sep = ""))
 
+print("##########################################")
+print("################Bigrams###################")
+print("##########################################")
+
+b.test.words <- c("the time", "time of", "of love", "love </s>")
+b.test.counter <- c(1, 4, 3, 20)
+b.test.expected.prob <- c(0.1, 0.2, 0.1, 0.5)
+
+
+b.test.input.words <- c("the time", "time of", "of love", "love </s>")
+for(i in 1:length(b.test.input.words)){
+    test.name <- paste("Test", i)
+    test.result <- bigrams.countForTerm(term = b.test.input.words[i], 
+                                              b.terms = b.test.words,b.counters = b.test.counter)
+    
+    test.passed <- (b.test.counter[i] == test.result)
+    print(paste(test.name, "::bigrams.countForTerm::", b.test.input.words[i], "::", test.passed , sep = ""))
+    if(!test.passed) stop()
+}
+
+
+b.test.input.words <- "doesNotExist"
+test.name <- "Test Error"
+is.error <- T
+test.result = tryCatch({
+    bigrams.countForTerm(term = b.test.input.words, 
+                         b.terms = b.test.words, b.counters = b.test.counter)
+    print(paste(test.name, "::bigrams.countForTerm::", b.test.input.words, "::", FALSE, sep = ""))
+    is.error <- F
+}, error = function(e) {
+    print(paste(test.name, "::bigrams.counterForTerm::", b.test.input.words, "::", TRUE, sep = ""))
+})
+
+print("##########################################")
+
+b.test.input.words <- c("the time", "time of", "of love", "love </s>")
+for(i in 1:length(b.test.input.words)){
+    test.name <- paste("Test", i)
+    test.result <- bigrams.probabilityForTerm(term = b.test.input.words[i], 
+                                              b.terms = b.test.words,b.counters = b.test.counter,
+                                              u.words = u.test.words, u.counters = u.test.counter)
+    test.passed <- (b.test.expected.prob[i] == test.result)
+    print(paste(test.name, "::bigrams.probabilityForTerm::", b.test.input.words[i], "::", test.passed , sep = ""))
+    if(!test.passed) stop()
+}
+
+
+b.test.input.words <- "doesNotExist"
+test.name <- "Test Error"
+is.error <- T
+test.result = tryCatch({
+    bigrams.probabilityForTerm(term = b.test.input.words, 
+                               b.terms = b.test.words, b.counters = b.test.counter,
+                               u.words = u.test.words, u.counters = u.test.counter)
+    print(paste(test.name, "::bigrams.probabilityForTerm::", b.test.input.words, "::", FALSE, sep = ""))
+    is.error <- F
+}, error = function(e) {
+    print(paste(test.name, "::bigrams.probabilityForTerm::", b.test.input.words, "::", TRUE, sep = ""))
+})
+
+if(!is.error) stop()
+
+print("##########################################")
+
+b.expected.word <- c("the", "time", "of", "love")
+b.expected.nextWord <- c("time", "of", "love", "</s>")
+
+test.name <- "Test"
+test.result <- bigrams.model(b.terms = b.test.words, b.counters = b.test.counter,
+                             u.words = u.test.words, u.counters = u.test.counter)
+check1 <- !is.null(test.result$b.word) & all(test.result$b.word == b.expected.word)
+check2 <- !is.null(test.result$b.nextWord) & all(test.result$b.nextWord == b.expected.nextWord)
+check3 <- !is.null(test.result$b.probability) & all(test.result$b.probability == b.test.expected.prob)
+test.passed <- check1 & check2 & check3
+print(paste(test.name, "::bigrams.model::", (check1 & check2 & check3), sep = ""))
+
+if(!test.passed) stop()
+
+print("##########################################")
+print("###############Trigrams###################")
 print("##########################################")
