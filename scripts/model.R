a32ebf8d9f495479e5db8c7555fa448240838458
Pier Lorenzo Paracchini <PPAR@statoil.com>
2016-05-03 21:43:33 +0200

    Trigram model with supporting functions & unit tests

--- scripts/model.R
+++ scripts/model.R
@@ -147,3 +147,75 @@ bigrams.model <- function(b.terms, b.counters, u.words, u.counters){
     
     result_ls <- list(b.word = result.word, b.nextWord = result.nextWord,  b.probability = result.prob)
 }
+
+########################
+####### TRIGRAMS #######
+########################
+
+trigrams.countForTerm <- function(term, t.terms, t.counters){
+    idx <- which(t.terms == term)
+    result <- 0
+    if(length(idx) == 1){
+        result <- t.counters[idx]
+    }else{
+        stop(paste("trigrams.countForTerm function::", term, "::not found", sep = ""))
+    }
+    result
+}
+
+#Get the probability for a trigrams
+# term -> the trigrams as "word_i-2 word_i-1 word_i"
+# $t.terms -> the list of trigrams (language model)
+# $t.counters -> the list of counts for each trigram
+# $b.terms -> the list of bigrams (language model)
+# $b.counters -> the list of counts for each bigram
+# Note! for bigram b.terms[i], respective count is b.counters[i]
+# Note! for trigram t.words[i], respective count is t.counters[i]
+
+# Return probability for the term (trigram)
+# The probability for term = "w_i-2 w_i-1 wi" is calculated as 
+# P(w_i|w_i-2, w_i-1) = count(w_i-2,w_i-1, w_i)/ count(w_i-2, w_i-1)
+
+trigrams.probabilityForTerm <- function(term, t.terms, t.counters, b.terms, b.counters){
+    result <- -1
+    idx <- which(t.terms == term)
+    tmp.words <- ngramTokenize(y = term, ng = 1)
+    
+    if(length(idx) == 1){
+        tmp.previous.bigram <- paste(tmp.words[1], tmp.words[2])
+        tmp.previous.bigram.count <- bigrams.countForTerm(term = tmp.previous.bigram, 
+                                                          b.terms = b.terms, b.counters = b.counters)
+        result <- t.counters[idx]/ tmp.previous.bigram.count
+    }else{
+        stop(paste("trigrams.probabilityForTerm function::", term, "::not found", sep = ""))
+    }
+    result
+}
+
+# Create the Trigram model as a list
+# $t.bigram -> the list of bigrams ("w_i-2 w_i-1")
+# $t.nextWord -> the list of word (w_i)
+# $t.probability -> the list of probabilities that the bigram is followed by that next word
+#
+# Note!! $t.probability[i] (probability) is related to "$t.bigram[i] $b.nextWord[i]"(bigram/ nextWord)
+# The probability for term = "w_i-2 w_i-1 wi" is calculated as 
+# P(w_i|w_i-2,w_i-1) = count(w_i-2, w_i-1, w_i)/ count(w_i-2, w_i-1)
+
+trigrams.model <- function(t.terms, t.counters, b.terms, b.counters){
+    size <- length(t.terms)
+    result.bigram <- character(size)
+    result.nextWord <- character(size)
+    result.prob <- numeric(size)
+    
+    for(i in 1:size){
+        tmp <- t.terms[i]
+        tmp.words <- ngramTokenize(y = tmp, ng = 1)
+        result.bigram[i] <- paste(tmp.words[1], tmp.words[2])
+        result.nextWord[i] <- tmp.words[3]
+        result.prob[i] <- trigrams.probabilityForTerm(term = tmp, 
+                                                     t.terms = t.terms, t.counters = t.counters,
+                                                     b.terms = b.terms, b.counters = b.counters)
+    }
+    
+    result_ls <- list(t.bigram = result.bigram, t.nextWord = result.nextWord,  t.probability = result.prob)
+}
--- scripts/model_test.R
+++ scripts/model_test.R
@@ -1,13 +1,12 @@
-rm(list = ls())
-source("model.R")
-
 print("##########################################")
 print("###############Unigrams###################")
 print("##########################################")
 
+rm(list = ls())
+source("model.R")
+
 u.test.words <- c("the", "time", "of", "love")
 u.test.counter <- c(10, 20, 30, 40)
-u.text.expected.prob <- c(0.1, 0.2, 0.3, 0.4)
 
 u.test.input.words <- c("the", "time", "of", "love")
 for(i in 1:length(u.test.input.words)){
@@ -32,6 +31,13 @@ test.result = tryCatch({
 if(!is.error) stop()
 
 print("##########################################")
+rm(list = ls())
+source("model.R")
+
+u.test.words <- c("the", "time", "of", "love")
+u.test.counter <- c(10, 20, 30, 40)
+u.text.expected.prob <- c(0.1, 0.2, 0.3, 0.4)
+
 
 u.test.input.words <- c("the", "time", "of", "love")
 for(i in 1:length(u.test.input.words)){
@@ -57,6 +63,13 @@ test.result = tryCatch({
 if(!is.error) stop()
 
 print("##########################################")
+rm(list = ls())
+source("model.R")
+
+u.test.words <- c("the", "time", "of", "love")
+u.test.counter <- c(10, 20, 30, 40)
+u.text.expected.prob <- c(0.1, 0.2, 0.3, 0.4)
+
 
 test.name <- "Test"
 test.result <- unigrams.model(u.words = u.test.words, u.counters = u.test.counter)
@@ -67,10 +80,11 @@ print(paste(test.name, "::unigrams.model::", (check1 & check2), sep = ""))
 print("##########################################")
 print("################Bigrams###################")
 print("##########################################")
+rm(list = ls())
+source("model.R")
 
 b.test.words <- c("the time", "time of", "of love", "love </s>")
 b.test.counter <- c(1, 4, 3, 20)
-b.test.expected.prob <- c(0.1, 0.2, 0.1, 0.5)
 
 
 b.test.input.words <- c("the time", "time of", "of love", "love </s>")
@@ -98,6 +112,15 @@ test.result = tryCatch({
 })
 
 print("##########################################")
+rm(list = ls())
+source("model.R")
+
+u.test.words <- c("the", "time", "of", "love")
+u.test.counter <- c(10, 20, 30, 40)
+
+b.test.words <- c("the time", "time of", "of love", "love </s>")
+b.test.counter <- c(1, 4, 3, 20)
+b.test.expected.prob <- c(0.1, 0.2, 0.1, 0.5)
 
 b.test.input.words <- c("the time", "time of", "of love", "love </s>")
 for(i in 1:length(b.test.input.words)){
@@ -127,6 +150,15 @@ test.result = tryCatch({
 if(!is.error) stop()
 
 print("##########################################")
+rm(list = ls())
+source("model.R")
+
+u.test.words <- c("the", "time", "of", "love")
+u.test.counter <- c(10, 20, 30, 40)
+
+b.test.words <- c("the time", "time of", "of love", "love </s>")
+b.test.counter <- c(1, 4, 3, 20)
+b.test.expected.prob <- c(0.1, 0.2, 0.1, 0.5)
 
 b.expected.word <- c("the", "time", "of", "love")
 b.expected.nextWord <- c("time", "of", "love", "</s>")
@@ -144,4 +176,100 @@ if(!test.passed) stop()
 
 print("##########################################")
 print("###############Trigrams###################")
+print("##########################################")
+rm(list = ls())
+source("model.R")
+
+t.test.words <- c("the time of", "time of love", "of love </s>")
+t.test.counter <- c(10, 20, 21)
+
+t.test.input.words <- c("the time of", "time of love", "of love </s>")
+
+for(i in 1:length(t.test.input.words)){
+    test.name <- paste("Test", i)
+    test.result <- trigrams.countForTerm(term = t.test.input.words[i], 
+                                        t.terms = t.test.words, t.counters = t.test.counter)
+    
+    test.passed <- (t.test.counter[i] == test.result)
+    print(paste(test.name, "::bigrams.countForTerm::", t.test.input.words[i], "::", test.passed , sep = ""))
+    if(!test.passed) stop()
+}
+
+
+t.test.input.words <- "doesNotExist"
+test.name <- "Test Error"
+is.error <- T
+test.result = tryCatch({
+    trigrams.countForTerm(term = t.test.input.words, 
+                         t.terms = t.test.words, t.counters = t.test.counter)
+    print(paste(test.name, "::trigrams.countForTerm::", t.test.input.words, "::", FALSE, sep = ""))
+    is.error <- F
+}, error = function(e) {
+    print(paste(test.name, "::trigrams.counterForTerm::", t.test.input.words, "::", TRUE, sep = ""))
+})
+if(!is.error) stop()
+
+print("##########################################")
+rm(list = ls())
+source("model.R")
+
+b.test.words <- c("the time", "time of", "of love", "love </s>")
+b.test.counter <- c(1, 4, 3, 20)
+
+t.test.words <- c("the time of", "time of love", "of love </s>")
+t.test.counter <- c(10, 20, 21)
+t.test.expected.prob <- c(10, 5, 7)
+
+t.test.input.words <- c("the time of", "time of love", "of love </s>")
+for(i in 1:length(t.test.input.words)){
+    test.name <- paste("Test", i)
+    test.result <- trigrams.probabilityForTerm(term = t.test.input.words[i],
+                                              t.terms = t.test.words, t.counters = t.test.counter, 
+                                              b.terms = b.test.words, b.counters = b.test.counter)
+    test.passed <- (t.test.expected.prob[i] == test.result)
+    print(paste(test.name, "::trigrams.probabilityForTerm::", t.test.input.words[i], "::", test.passed , sep = ""))
+    if(!test.passed) stop()
+}
+
+
+t.test.input.words <- "doesNotExist"
+test.name <- "Test Error"
+is.error <- T
+test.result = tryCatch({
+    trigrams.probabilityForTerm(term = t.test.input.words, 
+                               t.terms = t.test.words, t.counters = t.test.counter,
+                               b.terms = b.test.words, b.counters = b.test.counter)
+    print(paste(test.name, "::trigrams.probabilityForTerm::", t.test.input.words, "::", FALSE, sep = ""))
+    is.error <- F
+}, error = function(e) {
+    print(paste(test.name, "::trigrams.probabilityForTerm::", t.test.input.words, "::", TRUE, sep = ""))
+})
+
+if(!is.error) stop()
+
+print("##########################################")
+rm(list = ls())
+source("model.R")
+
+b.test.words <- c("the time", "time of", "of love", "love </s>")
+b.test.counter <- c(1, 4, 3, 20)
+
+t.test.words <- c("the time of", "time of love", "of love </s>")
+t.test.counter <- c(10, 20, 21)
+t.test.expected.prob <- c(10, 5, 7)
+
+t.expected.bigram <- c("the time", "time of", "of love")
+t.expected.nextWord <- c("of", "love", "</s>")
+
+test.name <- "Test"
+test.result <- trigrams.model(t.terms = t.test.words, t.counters = t.test.counter,
+                             b.terms = b.test.words, b.counters = b.test.counter)
+check1 <- !is.null(test.result$t.bigram) & all(test.result$t.bigram == t.expected.bigram)
+check2 <- !is.null(test.result$t.nextWord) & all(test.result$t.nextWord == t.expected.nextWord)
+check3 <- !is.null(test.result$t.probability) & all(test.result$t.probability == t.test.expected.prob)
+test.passed <- check1 & check2 & check3
+print(paste(test.name, "::trigrams.model::", (check1 & check2 & check3), sep = ""))
+
+if(!test.passed) stop()
+
 print("##########################################")
