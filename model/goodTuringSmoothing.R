9ba0765c9da886b9f42f1cc21e1ec86620bcb3ac
Pier Lorenzo Paracchini <pparacch.github@outlook.com>
2016-05-15 03:19:31 +0200

    simple GT linear Interpolation model with reduced data - separate folder

---
+++ model/goodTuringSmoothing.R
@@ -0,0 +1,44 @@
+# GOOD-TURING SMOOTHING
+#
+# Implementation of a (simple) Good Turing smoothing algorithm
+# based on simplification count_gt = count - 0.75
+
+
+# Calculate  the GoodTuring count (generalized for ngrams)
+# (i) term: the term looking for
+# (i) terms: the list of term available in our ngram domain
+# (i) counters: the frequency of the terms observed in our ngram domain
+#
+# (o) the good turing count (discounted count for the term)
+
+gts_getGoodTuringCount <- function(term, terms, counters){
+    #Simple implementation based on Good-Turing numbers
+    # print(paste("          >>>> term:", term))
+
+    result <- NULL
+    idx <- which(terms == term)
+    
+    
+    if(length(idx) == 0){
+        #A new term that I have never seen before so
+        result <- gts_support_getGoodTuringCountByActualCount(0)
+    }else{
+        result <- gts_support_getGoodTuringCountByActualCount(counters[idx])
+    }
+    return(result)
+}
+
+
+# Supporting method for the calculation of the Good-Turing Count
+
+gts_support_getGoodTuringCountByActualCount <- function(actualCount){
+    result <- NULL
+    if(actualCount == 0){
+        #A new term that I have never seen before so
+        result <- 0.000027
+    }else{
+        result <- actualCount - 0.75
+    }
+    # print(paste("          >>>> Actual count:", actualCount, "- Good-Turing Count:", result))
+    return(result)
+}
---
+++ model/model_simpleGTS_linearInterpolation_unitTests.R
@@ -0,0 +1,59 @@
+# dependent on:
+source("model_supportingFunctions.R")
+source("goodTuringSmoothing.R")
+source("model_simpleGTS_linearInterpolation.R")
+
+
+check.results <- function(input, expected, result){
+    print("  > Case")
+    print(paste("     Input:", input))
+    print(paste("  Expected:", expected))
+    print(paste("    Output:", result))
+    test.outcome <- abs(expected - unlist(result)) < 0.00000000001
+    if(!test.outcome)stop("Test: Failed")    
+}
+
+test.run <- function(test.name, test.inputs, test.expected, fun.udt,
+                     fun.lambda_1, fun.lambda_2, fun.lambda_3,
+                     fun.t.terms, fun.t.counts,
+                     fun.b.terms, fun.b.counts,
+                     fun.u.terms, fun.u.counts){
+    print(test.name)
+    result <- lapply(X = test.inputs, FUN = fun.udt, lambda_1 = fun.lambda_1, lambda_2 = fun.lambda_2, lambda_3 = fun.lambda_3,
+                     t.terms = fun.t.terms, t.counters = fun.t.counts,
+                     b.terms = fun.b.terms, b.counters = fun.b.counts,
+                     u.terms = fun.u.terms, u.counters = fun.u.counts)
+    
+    for(i in 1: length(test.inputs)){
+        check.results(test.inputs[i], test.expected[i], result[i])
+    }
+}
+
+cat("\n")
+test.name <- "Test - Linear Interpolation Model with Good-Turing Smoothing"
+test.inputs <- c("<s> i am", "going to hell")
+
+test.inputs.u.terms <- c("<s>", "i", "am", "happy", "</s>")
+test.inputs.u.counters <- c(10, 5, 2, 3, 10)
+
+test.inputs.b.terms <- c("<s> i", "i am", "am happy", "happy </s>")
+test.inputs.b.counters <- c(3, 1, 1, 1)
+
+test.inputs.t.terms <- c("<s> i am", "i am happy", "am happy </s>")
+test.inputs.t.counters <- c(3, 1, 1, 1)
+
+lambda_1 = 0.8; lambda_2 = 0.4; lambda_3 = 0.2
+
+test.expected <- c(
+    ((3 - 0.75) * lambda_1 / 6) + ((1 - 0.75) * lambda_2 / 6) + ((2 - 0.75) * lambda_3 / 30), #<s> i am
+    (0.000027 * lambda_1 / 6) + (0.000027 * lambda_2 / 6) + (0.000027 * lambda_3 / 30) #going to hell
+)
+
+test.run(test.name = test.name, test.inputs = test.inputs, test.expected = test.expected, 
+         fun.udt = trigrams.gtsmoothing.probabilityForTerm, 
+         fun.lambda_1 = lambda_1, fun.lambda_2 = lambda_2, fun.lambda_3 = lambda_3,
+         fun.t.terms = test.inputs.t.terms, fun.t.counts = test.inputs.t.counters,
+         fun.b.terms = test.inputs.b.terms, fun.b.counts = test.inputs.b.counters,
+         fun.u.terms = test.inputs.u.terms, fun.u.counts = test.inputs.u.counters)
+
+
